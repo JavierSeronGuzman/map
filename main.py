22837503a6722e3f97d704a33993d2679dfeb996
Javier Serón Guzmán <seron.JSG@gmail.com>
2024-04-26 02:48:24 +0000

    render

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
-import uvicorn
-from os import getenv
 
 
 
@@ -24,6 +22,3 @@ app.add_middleware(
 async def get_index():
     return FileResponse("static/index.html")
 
-if __name__ == "__main__":
-    port = int(getenv("PORT",8000))
-    uvicorn.run("main:app",host="0.0.0.0",port=port,reload=True)
